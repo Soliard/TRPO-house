06b3642ef39693287d65778178679fa35cedcfbd
Soliard <soliarder@gmail.com>
2021-03-24 12:26:20 +0700

    first commit

---
+++ request.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+
+url = 'http://localhost:5000/results'
+r = requests.post(url,json={'rate':5, 'sales_in_first_month':200, 'sales_in_second_month':400})
+
+print(r.json())
+
+
+# In[ ]:
+
+
+
+
